faa7448d64889352926ded1781068b63e7c4ac69
Jaman Shah <jamanshah5400@gmail.com>
2024-11-24 19:05:17 +0600

    bicycle interface created finished

---
+++ src/app/modules/bicycle/bicycle.interface.ts
@@ -0,0 +1,13 @@
+// bicycle types
+
+export type Bicycle = {
+  name: string;
+  brand: string;
+  price: string;
+  type: string;
+  description: string;
+  quantity: number;
+  inStock: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
